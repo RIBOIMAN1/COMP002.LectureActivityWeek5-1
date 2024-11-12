3d3a5a0dee09b2f6f6bdf8209e7512f388d51a1a
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-11-12 15:37:49 -0800

    script.js: - Changed the i value to equal to 0. - Changed the operating sign to < so that the function does not go out of bounds and output NaN, instead outputting the average of the numbers provided.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ myGrades = [100, 100, 90, 73, 78, 94, 86];
 
 myAverage = 0;
 
-for (let i = 1; i <= myGrades.length; i++) {
+for (let i = 0; i < myGrades.length; i++) { // Changed i to equal 0 since JavaScript values start from 0 instead of 1. Also, changed the operating sign to "<" so that it would only access the required code necessary to complete this function and the function would be in bounds.
 
 myAverage += myGrades[i];
 
